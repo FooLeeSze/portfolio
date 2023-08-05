eb29f3675ab7f9dff0cbd70a0f9bf447591bb758
Jaclyn-Foo <leeszefoo@gmail.com>
2023-08-06 01:42:55 +0800

    Update about and banner descriptions

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,24 +1,29 @@
 import React from "react";
-import '../Styles/About.css'
+import "../Styles/About.css";
 
 export default function About() {
+  return (
+    <section id="about-me" className="main-content">
+      <div id="about-section-title" className="section-title">
+        <h2>About me</h2>
+      </div>
+      <div id="personal-info-container">
+        <div id="personal-info">
+          <p>
+            I am a front-end engineer focused on front-end development using
+            React & React Native.
+          </p>
+          <p>Check out some of my work below!</p>
+        </div>
 
-    return (
-        <section id="about-me" className="main-content">
-            <div id="about-section-title" className="section-title">
-                <h2>About me</h2>
-            </div>
-            <div id="personal-info-container">
-                <div id="personal-info">
-                    <p>I am a PhD graduate and a self-taught web developer! My main focus is on front end development using React.</p>
-                    <p>I also have experience in algorithm scripting for scientific computation.</p>
-                </div>
-
-                <div id="prof-pic-container">
-                    <img src={require('../images/profile_pic.png')} alt="Portrait shot of me" id="profile-pic" />
-                </div>
-            </div>
-        </section>
-
-    )
+        <div id="prof-pic-container">
+          <img
+            src={require("../images/profile_pic.png")}
+            alt="Portrait shot of me"
+            id="profile-pic"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/Components/MainBanner.js
+++ src/Components/MainBanner.js
@@ -1,34 +1,42 @@
 import React from "react";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faAngleDown} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import { useTheme } from "../ThemeContext";
-import '../Styles/MainBanner.css'
+import "../Styles/MainBanner.css";
 
 export default function MainBanner() {
-    // Theme context
-    const darkMode = useTheme()
+  // Theme context
+  const darkMode = useTheme();
 
-    // Light and dark theme styles
-    const themeStyle = {
-        color: darkMode ? 'var(--banner-bg-color)' : 'var(--light-color)'
-    }
+  // Light and dark theme styles
+  const themeStyle = {
+    color: darkMode ? "var(--banner-bg-color)" : "var(--light-color)",
+  };
 
-    // Scroll to About Section upon button click
-    function scrollToElement(){;
-        const anchor = document.querySelector('#about-me');
-        anchor.scrollIntoView()
-    }
+  // Scroll to About Section upon button click
+  function scrollToElement() {
+    const anchor = document.querySelector("#about-me");
+    anchor.scrollIntoView();
+  }
 
-    return (
-        <section id="welcome-section">
-            <div id="welcome-content">
-                <h1>Hi, I'm <span id="name">Lee Sze.</span></h1>
-                <p>Web developer.</p>
+  return (
+    <section id="welcome-section">
+      <div id="welcome-content">
+        <h1>
+          Hi, I'm <span id="name">Lee Sze.</span>
+        </h1>
+        <p>Front-End Engineer</p>
 
-                <div id="down-btn-container">
-                    <span onClick={scrollToElement} style={themeStyle} id="scroll-down-btn"><FontAwesomeIcon icon={faAngleDown} /></span>
-                </div>
-            </div>
-        </section>
-    )
+        <div id="down-btn-container">
+          <span
+            onClick={scrollToElement}
+            style={themeStyle}
+            id="scroll-down-btn"
+          >
+            <FontAwesomeIcon icon={faAngleDown} />
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 }
